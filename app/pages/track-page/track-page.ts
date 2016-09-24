ba961e0c9def02010778cc7286cb5b21cd77f56d
AlonFisch <alfischer@att.net>
2016-09-24 03:09:36 -0400

    added basic tabs

---
+++ app/pages/track-page/track-page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the TrackPagePage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/track-page/track-page.html',
+})
+export class TrackPagePage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
