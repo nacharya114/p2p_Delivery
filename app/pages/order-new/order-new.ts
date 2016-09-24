a93fd6f494ebe2c5de91a737ab279b19adb71725
AlonFisch <alfischer@att.net>
2016-09-24 13:47:17 -0400

    order-new page added

---
+++ app/pages/order-new/order-new.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the OrderNewPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/order-new/order-new.html',
+})
+export class OrderNewPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
