33af9c7e272327f35aa99408e4521503f3af27bf
AlonFisch <alfischer@att.net>
2016-09-24 03:47:30 -0400

    tabs update

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { HomePage } from '../home/home';
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { OrderPagePage } from '../order-page/order-page';
+import { MapPagePage } from '../map-page/map-page';
+import { HistoryPagePage } from '../history-page/history-page';
+import {TrackPagePage} from '../track-page/track-page';
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -11,12 +12,14 @@ export class TabsPage {
   public tab1Root: any;
   public tab2Root: any;
   public tab3Root: any;
+  public tab4Root; any;
 
   constructor() {
     // this tells the tabs component which Pages
     // should be each tab's root Page
-    this.tab1Root = HomePage;
-    this.tab2Root = AboutPage;
-    this.tab3Root = ContactPage;
+    this.tab1Root = OrderPagePage;
+    this.tab2Root = MapPagePage;
+    this.tab3Root = HistoryPagePage;
+    this.tab4Root = TrackPagePage;
   }
 }
