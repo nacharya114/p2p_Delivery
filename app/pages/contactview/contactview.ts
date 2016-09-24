71cf9643fa7baf0a524a891c4ea0670fea9329ab
nacharya114 <nacharya114@gmail.com>
2016-09-24 03:13:50 -0400

    temp change to temp

---
+++ app/pages/contactview/contactview.ts
@@ -0,0 +1,10 @@
+mport { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/contactview/contactview.html'
+})
+export class ContactViewPage {
+  constructor(public navCtrl: NavController) {
+  }
+}
