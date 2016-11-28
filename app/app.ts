4e326341092c97f42282bfd17880e63446526fa4
nacharya114 <nacharya114@gmail.com>
2016-11-27 21:27:46 -0500

    Changed root page to login page

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Platform, ionicBootstrap } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
 import { TabsPage } from './pages/tabs/tabs';
+import { LoginPage } from './pages/login/login';
 import { ConnectivityService } from './providers/connectivity-service/connectivity-service';
 import { RequestDataProvider } from './providers/request-data-provider/request-data-provider';
 import { Params } from './providers/params/params';
@@ -16,7 +17,7 @@ export class MyApp {
   public rootPage: any;
 
   constructor(private platform: Platform) {
-    this.rootPage = TabsPage;
+    this.rootPage = LoginPage;
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
