8885d794e7c12e1e4edfdd791c54dc297c72dc9c
Andrew Wireman <AWIREMAN@lawn-128-61-118-19.lawn.gatech.edu>
2017-01-11 21:26:26 -0500

    new driver map page

---
+++ app/pages/driver-tab/driver-tab.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the DriverTabPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/driver-tab/driver-tab.html',
+})
+export class DriverTabPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
